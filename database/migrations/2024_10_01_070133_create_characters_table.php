27d8d64e2cecb6691c60683212769c2989c55e90
nolan694 <soulardnolan@gmail.com>
2024-10-01 10:42:06 +0200

    patch database

--- database/migrations/2024_10_01_070133_create_characters_table.php
+++ database/migrations/2024_10_01_070133_create_characters_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('characters',  function (Blueprint $table) {
+            $table->id();
             $table->float('price');
             $table->string('label');
             $table->string('desc');
