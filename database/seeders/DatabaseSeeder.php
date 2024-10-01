d8a72fb3393261aebe3f1f9a7ce8e997661e918d
nolan694 <soulardnolan@gmail.com>
2024-10-01 10:49:58 +0200

    add jeu  de donnés

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Character;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +14,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $c = new Character ;
+        $c->price = 54.99 ;
+        $c->label = 'tomioka';
+        $c ->desc = "pilier de l'eau"  ;
+        $c ->birth_date = '2001-08-20'  ;
+        $c ->image_path = 'https://static.wikia.nocookie.net/kimetsu-no-yaiba/images/5/55/Water_Breathing_%28Zenshuchuten%29-1-.png/revision/latest?cb=2020
+        0618200015&path-prefix=fr'  ;
+        $c -> save() ;
+
+        $c = new Character ;
+        $c->price = 15.99  ;
+        $c->label = 'tanjiro' ;
+        $c ->desc = 'trop fort' ;
+        $c ->birth_date = '1996-03-27' ;
+        $c ->image_path = 'https://demon-slayer.fr/wp-content/uploads/2022/07/zenitsu-demon-slayer-png.png' ;                                            
+        $c -> save() ;
+
+        $c = new Character ;
+        $c->price = 20.99  ;
+        $c->label = 'zenitsu' ;
+        $c ->birth_date = '2001-11-07' ;
+        $c ->image_path = 'https://demon-slayer.fr/wp-content/uploads/2022/07/zenitsu-demon-slayer-png.png' ;
+        $c -> save() ;
+
+
+
+         // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
@@ -21,3 +48,4 @@ class DatabaseSeeder extends Seeder
         ]);
     }
 }
+
